6b77632b925653df01f4f29e584c408408ede176
Samatar Xasan <Samatar.Xasan@gmail.com>
2020-12-02 21:01:19 +0000

    Completed project

---
+++ script.js
@@ -0,0 +1,15 @@
+const panels = document.querySelectorAll('.panel');
+const active = document.querySelector('.active');
+
+panels.forEach((panel)=>{
+    panel.addEventListener('click',() => {
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
+
+function removeActiveClasses() {
+    panels.forEach((panel) => {
+        panel.classList.remove('active')
+    })
+}
